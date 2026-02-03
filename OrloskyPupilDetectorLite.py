09e2d1c27c52a0bc0ed22755f2e82f09bbdd3690
Emanuele <altamuraemanuele@yahoo.it>
2026-02-03 23:46:33 +0100

    warning di OpenCV/FFmpeg. La linea

--- OrloskyPupilDetectorLite.py
+++ OrloskyPupilDetectorLite.py
@@ -4,6 +4,7 @@ import tkinter as tk
 import os
 from tkinter import filedialog
 
+
 # Crop the image to maintain a specific aspect ratio (width:height) before resizing.
 def crop_to_aspect_ratio(image, width=640, height=480):
     current_height, current_width = image.shape[:2]
@@ -23,11 +24,13 @@ def crop_to_aspect_ratio(image, width=640, height=480):
 
     return cv2.resize(cropped_img, (width, height))
 
+
 # Apply thresholding to an image
 def apply_binary_threshold(image, darkestPixelValue, addedThreshold):
     threshold = darkestPixelValue + addedThreshold
     _, thresholded_image = cv2.threshold(image, threshold, 255, cv2.THRESH_BINARY_INV)
     return thresholded_image
+
 
 # Finds a square area of dark pixels in the image
 def get_darkest_area(image):
@@ -59,6 +62,7 @@ def get_darkest_area(image):
 
     return darkest_point
 
+
 # Mask all pixels outside a square defined by center and size
 def mask_outside_square(image, center, size):
     x, y = center
@@ -71,6 +75,7 @@ def mask_outside_square(image, center, size):
     bottom_right_y = min(image.shape[0], y + half_size)
     mask[top_left_y:bottom_right_y, top_left_x:bottom_right_x] = 255
     return cv2.bitwise_and(image, mask)
+
 
 # Returns the largest contour that is not extremely long or tall
 def filter_contours_by_area_and_return_largest(contours, pixel_thresh, ratio_thresh):
@@ -88,6 +93,7 @@ def filter_contours_by_area_and_return_largest(contours, pixel_thresh, ratio_thr
                     largest_contour = contour
 
     return [largest_contour] if largest_contour is not None else []
+
 
 # Process frames for pupil detection
 def process_frames(thresholded_image_medium, frame, gray_frame, darkest_point, debug_mode_on, render_cv_window):
@@ -113,6 +119,7 @@ def process_frames(thresholded_image_medium, frame, gray_frame, darkest_point, d
 
     return final_rotated_rect
 
+
 # Process a single frame for pupil detection
 def process_frame(frame):
     frame = crop_to_aspect_ratio(frame)
@@ -122,6 +129,7 @@ def process_frame(frame):
     thresholded_image_medium = apply_binary_threshold(gray_frame, darkest_pixel_value, 15)
     thresholded_image_medium = mask_outside_square(thresholded_image_medium, darkest_point, 250)
     return process_frames(thresholded_image_medium, frame, gray_frame, darkest_point, False, False)
+
 
 # Process video frames for pupil detection
 def process_video(video_path, input_method):
@@ -151,15 +159,18 @@ def process_video(video_path, input_method):
     out.release()
     cv2.destroyAllWindows()
 
+
 # Prompt user to select a video file
 def select_video():
     root = tk.Tk()
     root.withdraw()
-    video_path = 'C:/Storage/Google Drive/Eye Tracking/fulleyetest3.mp4'
+    base_url = os.getcwd()
+    video_path = base_url + '/eye_test.mp4'
     if not os.path.exists(video_path):
         video_path = filedialog.askopenfilename(filetypes=[("Video Files", "*.mp4;*.avi")])
     if video_path:
         process_video(video_path, 1)
+
 
 if __name__ == "__main__":
     select_video()
